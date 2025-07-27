a162c6ed7d4c2fb424e5ed0fbe9cc550470aed2a
tskaushal <66420398+tskaushal@users.noreply.github.com>
2025-07-27 22:10:53 +0530

    Changed AMI of server

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+  ami           = "ami-042b4708b1d05f512" # Amazon Linux 2 AMI
   instance_type = "t3.micro"
 
   tags = {
